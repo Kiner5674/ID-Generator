442c30dfd42a201f433c40d3a6c39f91a804eab7
Kiner5674 <hello1010@outlook.jp>
2022-03-27 13:06:18 +0900

    プロジェクト ファイルを追加します。

---
+++ ID-Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+string digit = "";
+Console.WriteLine("digit:");
+while (true)
+{
+    try
+    {
+        digit = Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        continue;
+    }
+    if (digit != null)
+    {
+        break;
+    }
+}
+string letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //62
+Random random = new Random();
+string ID = "";
+for (int i = 0; i < int.Parse(digit); i++)
+{
+    ID += letter[random.Next(1, 63)];
+}
+Console.WriteLine(ID);

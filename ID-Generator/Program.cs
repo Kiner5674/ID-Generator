4c64f90ef5cc3242c2cb282d90be767228f4826c
Kiner5674 <hello1010@outlook.jp>
2022-03-28 16:23:34 +0900

    repeat

--- ID-Generator/Program.cs
+++ ID-Generator/Program.cs
@@ -1,5 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Declaration system
 string digit = "";
+string repeat = "";
+string letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-[]:;,./<>?_+*}`{=~|"; //82 letter
+Random random = new Random();
+string ID = "";
+
+//Output system
 Console.WriteLine("digit:");
 while (true)
 {
@@ -11,22 +17,39 @@ while (true)
     {
         continue;
     }
-    if (digit != null)
+    if (digit != null | digit.GetType() == typeof(int))
     {
         break;
     }
 }
-string letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //62 letter
-Random random = new Random();
-string ID = "";
-for (int rep = 0; rep < 10; rep++)
+
+Console.WriteLine("repeat:");
+while (true)
+{
+    try
+    {
+        repeat = Console.ReadLine();
+    }
+    catch (Exception)
+    {
+        continue;
+    }
+    if (repeat != null | repeat.GetType() == typeof(int))
+    {
+
+    }
+}
+
+//Generating system
+for (int rep = 1; rep < int.Parse(repeat); rep++)
 {
     for (int i = 0; i < int.Parse(digit); i++)
     {
-        ID += letter[random.Next(1, 62)];
+        ID += letter[random.Next(1, 82)];
     }
     Console.WriteLine(ID + "\n");
     ID = "";
 }
+
 Console.WriteLine("\nPress any key........");
 Console.ReadKey();
